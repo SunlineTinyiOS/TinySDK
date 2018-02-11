95959a907eea80639b0f03e2109590c9b9e0d018
kingdomrain <bygd2014@sina.com>
2018-02-11 14:02:05 +0800

    Commit
    Add TinySDK class

---
+++ TinySDK/Classes/TinySDK.h
@@ -0,0 +1,13 @@
+//
+//  TinySDK.h
+//  TinySDK
+//
+//  Created by kingdomrain on 2018/2/11.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TinySDK : NSObject
+
+-(void)version;
+@end
---
+++ TinySDK/Classes/TinySDK.m
@@ -0,0 +1,17 @@
+//
+//  TinySDK.m
+//  TinySDK
+//
+//  Created by kingdomrain on 2018/2/11.
+//
+
+#import "TinySDK.h"
+
+@implementation TinySDK
+
+-(void)version{
+    NSLog(@"print TinySDK version");
+}
+
+
+@end
