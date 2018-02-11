2e32480f7bbb485ef88d5bb37bba63b4a5f81ff9
kingdomrain <bygd2014@sina.com>
2018-02-11 17:42:46 +0800

    Commit
    Commit

--- TinySDK/Classes/TinySDK.h
+++ TinySDK/Classes/TinySDK.h
@@ -6,6 +6,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "TFManager.h"
 
 @interface TinySDK : NSObject
 
---
+++ TinySDK/Classes/include/TFManager.h
@@ -0,0 +1,22 @@
+//
+//  TinyFrameManager.h
+//  TinyFrame
+//
+//  Created by xiangfp on 15/11/5.
+//  Copyright © 2015年 Sunline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TFManager : UIViewController
+
++ (TFManager *)tfManager;
+
+- (void)openFrameWithURL:(NSString *)url;
+
+-(void)didAppearPage;
+
+
+
+
+@end
